3e15c988d328cd1499ca3042e8816bdad8af8a59
Alvaro Pereda <alvaro.pereda.sancho@teamaol.com>
2013-10-17 23:48:07 +0100

    Test solved.

--- src/ie/alvarop/sample/count/test/CountTest.java
+++ src/ie/alvarop/sample/count/test/CountTest.java
@@ -21,12 +21,12 @@ public class CountTest {
 	@Test
 	public void test() {
 		CountIt countit = new CountIt();
-		String result = countit.resolve("[]");
-		assertTrue(result.equalsIgnoreCase("YES"));
-		result = countit.resolve("[5,?]");
+		String result = countit.resolve("[5,?]");
 		assertTrue(result.equalsIgnoreCase("YES"));
 		result = countit.resolve("[[5,?],?]");
-		assertTrue(result.equalsIgnoreCase("10"));
+		assertTrue(result.equalsIgnoreCase("YES"));
+		result = countit.resolve("[[5,?],?]");
+		assertTrue(result.equalsIgnoreCase("YES"));
 	}
 	
 	@Test
@@ -34,6 +34,8 @@ public class CountTest {
 		CountIt countit = new CountIt();
 		int commaPos = countit.getCommaPos("[5,?]", 0);
 		assertTrue(commaPos == 2);
+		commaPos = countit.getCommaPos("[[5,?],6]", 3);
+		assertTrue("Received value " + commaPos, commaPos == 6);
 	}
 
 	@Test
@@ -55,18 +57,59 @@ public class CountTest {
 		assertTrue(reduc.equalsIgnoreCase("20"));
 		reduc = countit.reduce("[10,??]");
 		assertTrue(reduc.equalsIgnoreCase("20"));
+		reduc = countit.reduce("[20,10]");
+		assertTrue(reduc.equalsIgnoreCase("NO"));
+		reduc = countit.reduce("[10,????]");
+		assertTrue(reduc.equalsIgnoreCase("NO"));
 		reduc = countit.reduce("[????,10]");
-		assertTrue(reduc.equalsIgnoreCase("NO"));		
+		assertTrue(reduc.equalsIgnoreCase("NO"));
+		reduc = countit.reduce("[????,20]");
+		assertTrue(reduc.equalsIgnoreCase("40"));
 	}
 	
 	@Test
 	public void testRange() {
 		CountIt countit = new CountIt();
 		String range = countit.range("[5,?]", 2);
-		assertTrue(range.equalsIgnoreCase("[5,?]"));
+		assertTrue("Received range was " + range, range.equalsIgnoreCase("[5,?]"));
 		range = countit.range("[[5,?],?]", 3);
 		assertTrue(range.equalsIgnoreCase("[5,?]"));
 		range = countit.range("[[5,?],?]", 6);
 		assertTrue(range == null);
+		range = countit.range("[[10,?],?]", 3);
+		assertTrue(range.equalsIgnoreCase("[10,?]"));
+		range = countit.range("[[10,???],?]", 3);
+		assertTrue("Received range was " + range, range.equalsIgnoreCase("[10,???]"));
+	}
+	
+	@Test
+	public void testNextRange() {
+		CountIt countit = new CountIt();
+		String range = countit.nextRange("[5,?]", 0);
+		assertTrue("Received range was ", range.equalsIgnoreCase("[5,?]"));
+		range = countit.nextRange("[[5,?],?]", 0);
+		assertTrue("Received range was ", range.equalsIgnoreCase("[5,?]"));
+		range = countit.nextRange("[[5,?],?]", 3);
+		assertTrue("Received range was ", range == null);
+	}
+	
+	@Test
+	public void testReplace() {
+		CountIt countit = new CountIt();
+		String replaced = countit.replace("[[5,?],?]", "[5,?]", "10");
+		assertTrue("Received replaced " + replaced, replaced.equalsIgnoreCase("[10,?]"));
+		replaced = countit.replace("[[?,[5,?]],?]", "[5,?]", "10");
+		assertTrue("Received replaced " + replaced, replaced.equalsIgnoreCase("[[?,10],?]"));
+	}
+	
+	@Test
+	public void testOneRun() {
+		CountIt countit = new CountIt();
+		String result = countit.oneRun("[[5,?],?]");
+		assertTrue("Received resilt " + result, result.equalsIgnoreCase("[10,?]"));
+		result = countit.oneRun("[10,?]");
+		assertTrue("Received resilt " + result, result.equalsIgnoreCase("YES"));
+		result = countit.oneRun("[[5,2],?]");
+		assertTrue("Received resilt " + result, result.equalsIgnoreCase("NO"));
 	}
 }
